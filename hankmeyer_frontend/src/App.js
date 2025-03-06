738e8a61d93ad3f7c0406ab8e3e9f380ea0b6097
CanoeCanuck <hendrikreginald@gmail.com>
2025-03-06 16:54:50 -0500

    O Canada

--- hankmeyer_frontend/src/App.js
+++ hankmeyer_frontend/src/App.js
@@ -29,13 +29,17 @@ function App() {
       <Route path='/contact' element={<Contact/>} />
     </Routes>
     <footer className='d-flex justify-content-center bg-dark text-white'>
-    <small><i class="bi bi-c-circle"></i> 2023 - Hank Meyer -&nbsp;
+    <small>
+    <a href="https://whc.ca/hosted-in-canada/?aff=3153&gbid=1en" onclick="window.open(this.href, 'popupWindow', 'width=450, height=695, status=no, scrollbars=no, menubar=no'); return false;"><img src="https://s.whc.ca/badges/proudly-canadian-badge.svg" width="130" alt="Canadian Badge" /></a>
+    &nbsp; <i class="bi bi-c-circle"></i> 2025 - Hank Meyer -&nbsp;
       <a href="https://www.linkedin.com/in/hank-meyer-1aa156127" target="_blank" className="footerlinks" ><i className="bi bi-linkedin" fill="currentcolor"> </i></a>
       <a href="https://www.facebook.com/HankMeyer.ca/" target="_blank" className="footerlinks"><i className="bi bi-facebook"> </i></a>
       <a href="https://twitter.com/Canoe_Canuck" target="_blank" className="footerlinks"><i className="bi bi-twitter"> </i></a>
       <a href="https://www.instagram.com/canoe_canuck/" target="_blank" className="footerlinks"><i className="bi bi-instagram"> </i></a>
       <a href="https://www.youtube.com/@CanoeCanuck" target="_blank" className="footerlinks"><i className="bi bi-youtube"> </i></a>
       <a href="https://github.com/CanoeCanuck" target="_blank" className="footerlinks"><i className="bi bi-github"> </i></a>
+      &nbsp;
+      <a href="https://whc.ca/hosted-in-canada/?aff=3153&gbid=2en" onclick="window.open(this.href, 'popupWindow', 'width=450, height=695, status=no, scrollbars=no, menubar=no'); return false;"><img src="https://s.whc.ca/badges/hosted-in-canada-badge.svg" width="110" alt="Canadian Badge" /></a>
     </small>
 </footer>
     </BrowserRouter>
